fa798d68cd92a7591241ca9e0b70af3b57f4b180
mutabazichristian <c.mutabazi@alustudent.com>
2023-11-22 09:31:52 +0200

    gutting the front end && fixing import issues in backend

---
+++ server/controllers/prompterController.js
@@ -0,0 +1,7 @@
+
+const signup = (req, res) => {
+    console.log('this it the sign up controller');
+    res.json({ message: 'this is the singup controller' })
+}
+
+module.exports = { signup };
--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const sequelize = require('./config/database');
+const prompterRouter = require('./routes/prompterRouter');
 
 //consts
 PORT = process.env.PORT || 8081;
 
 const app = express();
 app.use(cors);
+app.use(prompterRouter);
 
 //trying the sequelize connection
 sequelize.authenticate()

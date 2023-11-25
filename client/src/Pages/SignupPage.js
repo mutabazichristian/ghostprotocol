7189c64d44cfcc3826b7d42829bc5df3d4ea1350
mutabazichristian <c.mutabazi@alustudent.com>
2023-11-25 14:42:24 +0200

    bunch of stuff

--- client/src/Pages/SignupPage.js
+++ client/src/Pages/SignupPage.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function SignupPage() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [reapeatPassword, setRepeatePassword] = useState('');
+
     const handleSubmit = () => {
-        console.log('tryna submit huh');
+        console.log('tryna submit the values huh?',name,email,password,reapeatPassword);
     }
     return (
         <div>
-            <form action=""></form>
             <p>this is the sign in page</p>
+            <form onSubmit={handleSubmit}>
+                <table>
+                    <tr>
+                        <td><label htmlFor="">Name </label></td>
+                        <td><input type="text" name='name' onChange={e => { setName(e.target.value) }} /></td>
+                    </tr>
+                    <tr>
+                        <td><label htmlFor="">Email</label></td>
+                        <td><input type="email" name="email" id="signup-email" onChange={e => setEmail(e.target.value)} /></td>
+                    </tr>
+                    <tr>
+                        <td><label htmlFor="">Password</label></td>
+                        <td><input type="password" name='password' onChange={e => setPassword(e.target.value)} /></td>
+                    </tr>
+                    <tr>
+                        <td><label htmlFor="">Repeat Password</label></td>
+                        <td><input type="password" name='reapeatPassword' onChange={e => setRepeatePassword(e.target.value)} /></td>
+                    </tr>
+                </table>
+            </form>
         </div>
     );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,4 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from 'dotenv/config';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';

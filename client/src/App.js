fca93df11afd601f513ca7e240621c936dee7b2f
mutabazichristian <c.mutabazi@alustudent.com>
2023-11-25 07:26:44 +0200

    lets go

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LangingPage from './Pages/LangingPage';
+import SignupPage from './Pages/SignupPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<LangingPage />} />
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/' element={<SignupPage />} />
       </Routes>
     </BrowserRouter>
   );
